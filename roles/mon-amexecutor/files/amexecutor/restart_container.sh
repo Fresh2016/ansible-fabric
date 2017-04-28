dc562fe9bb19b7d87e91bc3867e92e523ee1b3e7
liucl <liuchenglong3@jd.com>
2017-04-28 16:07:14 +0800

    更新amexecutor的测试脚本

--- roles/mon-amexecutor/files/amexecutor/restart_container.sh
+++ roles/mon-amexecutor/files/amexecutor/restart_container.sh
@@ -3,3 +3,15 @@ echo    ">> This is a test script, doing nothing <<"
 echo    "------------------------------------------"
 echo -e "\n\n$(env | grep ^AMX)\n\n"
 echo -e "------------------------------------------\n\n\n"
+
+CNT=1
+while [ ${CNT} -le ${AMX_ALERT_LEN} ]; do
+    echo "  >> processing the ${CNT} alert ..."
+    eval "LABEL_ACTION=\${AMX_ALERT_${CNT}_LABEL_action}"
+    eval "LABEL_INSTANCE=\${AMX_ALERT_${CNT}_LABEL_instance}"
+    eval "LABEL_NAME=\${AMX_ALERT_${CNT}_LABEL_name}"
+    echo "     AMX_ALERT_${CNT}_LABEL_action is: ${LABEL_ACTION}"
+    echo "     Restarting ${LABEL_NAME} on ${LABEL_INSTANCE}"
+    echo "  >> process finished"
+    ((CNT+=1))
+done
