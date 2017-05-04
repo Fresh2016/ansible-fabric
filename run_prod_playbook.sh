0f53df1eb3d6ed701ab10a90179fd47b61115a6f
liucl <liuchenglong3@jd.com>
2017-05-04 10:34:47 +0800

    去掉peer的privileged权限，修改所有fabric监听为域名

--- run_prod_playbook.sh
+++ run_prod_playbook.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-if [[ "$1" == "-vvv"  ]]; then
-   OPS=$1
+OPTS=${OPTS:='-C'}
+if [[ $1 ]]; then
+    OPTS=$1
 fi
-if [[ "$1" == "-C" ]]; then
-   OPS=$1
-fi
-ansible-playbook $OPS -i inventories/prod/hosts prod.yml
+
+case ${OPTS} in
+    -vvv)
+        ansible-playbook $OPTS -i inventories/prod/hosts prod.yml
+        ;;
+    -C)
+        ansible-playbook $OPTS -i inventories/prod/hosts prod.yml
+        ;;
+    -p|-P)
+        printf "\n\n\n<<<running prod_prepare.yml>>>\n\n\n"
+        ansible-playbook -i inventories/prod/hosts prod_prepare.yml
+        printf "\n\n\n<<<running prod.yml>>>\n\n\n"
+        ansible-playbook -i inventories/prod/hosts prod.yml
+        ;;
+    -d|-D)
+        ansible-playbook -i inventories/prod/hosts prod.yml
+        ;;
+    *)
+        echo "Only 1 options accept: -vvv, -C, -p|-P, -d|-D"
+        echo "    - vvv,    verbose output"
+        echo "    - C,      dry run, check only, DEFAULT choice"
+        echo "    - p|-P,   run prod_prepare.yml before running prod.yml"
+        echo "    - d|-D,   ONLY run prod.yml"
+        ;;
+esac
